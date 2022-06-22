d9408cd20c15a2c85f379b045a0ac3019e589f6d
walentik39 <107572801+walentik39@users.noreply.github.com>
2022-06-22 18:42:02 +0300

    Update Program.cs
    обновил

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyConsoleApplication
+{
+    class MyFirstClass
+    {
+       static void  Main(string[] args)
+       {
+        System.Console.WriteLine("Hello,");
+        Console.WriteLine("World!");
+
+        Console.ReadLine();
+
+       }
+
+    }
+
+}
+
